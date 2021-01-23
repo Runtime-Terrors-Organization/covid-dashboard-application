373b439d126e41a42c9bc96eb06e327c8e8c1bae
Nathan <nathaniel.mccaw@gmail.com>
2021-01-23 16:25:35 -0500

    added google map to application

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,39 @@
 var todaysDate = moment().format('YYYY[-]MM[-]DD');
 console.log(todaysDate);
 
+let map;
 
-
-
-
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8,
+    });
+}
 
 // Proxy Url deals with a CORS issue when using the Covid API
 var proxyUrl = "https://sheltered-ocean-70759.herokuapp.com/"
 var apiUrl = "https://api.covid19tracker.ca/reports/province/on";
 
-	fetch(proxyUrl + apiUrl)
-		.then(function(response) {
-			if (response.ok) {
-				response.json().then(function(text) {
-					console.log(text);
-					console.log(text.data.length);
-					//For loop to compare current date to date in API
-					
-					for(var i = 360; i < text.data.length; i++) {
-						var dates = text.data[i].date;
-						console.log(dates);
-						if ( dates === todaysDate) {
-							console.log("matched")
-						}
-					}
-					
-					
-				})
-			}
-		})
-	
+fetch(proxyUrl + apiUrl)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(text) {
+                console.log(text);
+                console.log(text.data.length);
+                //For loop to compare current date to date in API
+
+                for (var i = 360; i < text.data.length; i++) {
+                    var dates = text.data[i].date;
+                    console.log(dates);
+                    if (dates === todaysDate) {
+                        console.log("matched")
+                    }
+                }
+
+
+            })
+        }
+    })
