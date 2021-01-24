9b93922b663c88bfd2a8328e20aceef01b26a2a9
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-01-24 13:42:14 -0500

    Update main.js
    Ensured that if/else statement in dropdown remains.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,43 @@
 let map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: {
-            lat: -34.397,
-            lng: 150.644
-        },
-        zoom: 8,
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+  zoom: 8,
+  });
 }
 
 var todaysDate = moment().format('YYYY[-]MM[-]DD');
 console.log(todaysDate);
 
 
-
-
-
-
 // Proxy Url deals with a CORS issue when using the Covid API
 var proxyUrl = "https://sheltered-ocean-70759.herokuapp.com/"
 var apiUrl = "https://api.covid19tracker.ca/reports/province/on";
 
-	fetch(proxyUrl + apiUrl)
-		.then(function(response) {
-			if (response.ok) {
-				response.json().then(function(text) {
-					console.log(text);
-					console.log(text.data.length);
-					//For loop to compare current date to date in API
-					
-					for(var i = 360; i < text.data.length; i++) {
-						var dates = text.data[i].date;
-						console.log(dates);
-						if ( dates === todaysDate) {
-							console.log("matched")
-						}
+fetch(proxyUrl + apiUrl)
+	.then(function(response) {
+	  if (response.ok) {
+		  response.json().then(function(text) {
+				console.log(text);
+				console.log(text.data.length);
+				//For loop to compare current date to date in API
+				
+				for(var i = 360; i < text.data.length; i++) {
+					var dates = text.data[i].date;
+					console.log(dates);
+					if ( dates === todaysDate) {
+						console.log("matched")
 					}
-					
-					
-				})
-			}
-    });
-    
+				}
+			})
+		}
+  });
+
+  
 function dropDown() {
   var apiUrlRegions = "https://api.covid19tracker.ca/regions";
 	fetch(proxyUrl + apiUrlRegions)
@@ -51,14 +46,17 @@ function dropDown() {
 				response.json().then(function(text) {
 					console.log(text);
 
-				for(var i = 0; i < text.data.length; i++) {
-          $(".region-dropdown").append(`
-          <option value=${text.data[i].hr_uid}>${text.data[i].engname}</option>
-          `)
-        };
-      })
-    }
-  });
+          for (var i = 0; i < text.data.length; i++) {
+            var unitID = text.data[i].hr_uid;
+            if (unitID === 3553 || unitID === 3570 || unitID === 3595) {
+                $(".region-dropdown").append(`<option value=${text.data[i].hr_uid}>${text.data[i].engname}</option>`)
+            } else {
+                $(".region-dropdown").append(`<option value=${text.data[i].hr_uid} disabled >${text.data[i].engname}</option>`)
+            };
+          }
+        })
+      }
+    });
 };
 
 function provincialData() {
