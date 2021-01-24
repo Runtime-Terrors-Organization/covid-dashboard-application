1b295730382610548400da38f9046608774e472c
Nathan <nathaniel.mccaw@gmail.com>
2021-01-24 15:22:06 -0500

    hide regional options not available to choose

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
-    },
-  zoom: 8,
-  });
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 43.9009643,
+            lng: -79.8026284
+        },
+        zoom: 8,
+    });
 }
 
 var todaysDate = moment().format('YYYY[-]MM[-]DD');
@@ -19,110 +19,113 @@ var proxyUrl = "https://sheltered-ocean-70759.herokuapp.com/"
 var apiUrl = "https://api.covid19tracker.ca/reports/province/on";
 
 fetch(proxyUrl + apiUrl)
-	.then(function(response) {
-    if (response.ok) {response.json().then(function(text) {
-				console.log(text);
-				console.log(text.data.length);
-				//For loop to compare current date to date in API
-				
-				for(var i = 360; i < text.data.length; i++) {
-					var dates = text.data[i].date;
-					console.log(dates);
-					if ( dates === todaysDate) {
-						console.log("matched")
-					}
-				}
-			})
-		}
-  });
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(text) {
+                console.log(text);
+                console.log(text.data.length);
+                //For loop to compare current date to date in API
 
-  
-function dropDown() {
-  var apiUrlRegions = "https://api.covid19tracker.ca/regions";
-	fetch(proxyUrl + apiUrlRegions)
-		.then(function(response) {
-			if (response.ok) {
-				response.json().then(function(text) {
-					console.log(text);
-
-          for (var i = 0; i < text.data.length; i++) {
-            var unitID = text.data[i].hr_uid;
-            if (unitID === 3553 || unitID === 3570 || unitID === 3595) {
-                $(".region-dropdown").append(`<option value=${text.data[i].hr_uid}>${text.data[i].engname}</option>`)
-            } else {
-                $(".region-dropdown").append(`<option value=${text.data[i].hr_uid} disabled >${text.data[i].engname}</option>`)
-            };
-          }
-        })
-      }
+                for (var i = 360; i < text.data.length; i++) {
+                    var dates = text.data[i].date;
+                    console.log(dates);
+                    if (dates === todaysDate) {
+                        console.log("matched")
+                    }
+                }
+            })
+        }
     });
+
+
+function dropDown() {
+    var apiUrlRegions = "https://api.covid19tracker.ca/regions";
+    fetch(proxyUrl + apiUrlRegions)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(text) {
+                    console.log(text);
+
+                    for (var i = 0; i < text.data.length; i++) {
+                        var unitID = text.data[i].hr_uid;
+                        if (unitID === 3553 || unitID === 3570 || unitID === 3595) {
+                            $(".region-dropdown").append(`<option value=${text.data[i].hr_uid}>${text.data[i].engname}</option>`)
+                        } else {
+                            $(".extraRegionContainer").append(`<option value=${text.data[i].hr_uid}>${text.data[i].engname}</option>`)
+                        };
+                    }
+                })
+            }
+        });
 };
 
 function provincialData() {
-  var apiUrlProvincialData = `https://api.covid19tracker.ca/reports/province/on`;
-  fetch(proxyUrl + apiUrlProvincialData)
-  .then(function(response) {
-    if (response.ok) {
-      response.json().then(function(text) {
-        console.log(text);
+    var apiUrlProvincialData = `https://api.covid19tracker.ca/reports/province/on`;
+    fetch(proxyUrl + apiUrlProvincialData)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(text) {
+                    console.log(text);
 
-        for(var i = 360; i < text.data.length; i++) {
-          var date = text.data[i].date;
-          if ( date === todaysDate) {
-            console.log(text.data[i]);
-            $(".total-recoveries").append(`${text.data[i].total_recoveries}`);
-            $(".total-vaccinations").append(`${text.data[i].total_vaccinations}`);
-            $(".total-vaccinated").append(`${text.data[i].total_vaccinated}`);
-          };
-        };
-      })
-    };
-  })
+                    for (var i = 360; i < text.data.length; i++) {
+                        var date = text.data[i].date;
+                        if (date === todaysDate) {
+                            console.log(text.data[i]);
+                            $(".total-recoveries").append(`${text.data[i].total_recoveries}`);
+                            $(".total-vaccinations").append(`${text.data[i].total_vaccinations}`);
+                            $(".total-vaccinated").append(`${text.data[i].total_vaccinated}`);
+                        };
+                    };
+                })
+            };
+        })
 };
 
 $(".region-dropdown").on("change", displayData);
 
 function displayData() {
-  var locationId = $(this).val();
 
-  var apiUrlRegions = "https://api.covid19tracker.ca/regions";
-  fetch(proxyUrl + apiUrlRegions)
-	.then(function(response) {
-		if (response.ok) {
-			response.json().then(function(text) {
-        console.log(text);
-        for (var i = 0; i < text.data.length; i++) {
-          if (text.data[i].hr_uid == locationId) {
-          $(".region-title").append(`${text.data[i].engname}`)
-          };
-        }
-      });
-    };
-  })
+    var locationId = $(this).val();
 
-  var apiUrlRegionData = `https://api.covid19tracker.ca/reports/regions/${locationId}`;
-  fetch(proxyUrl + apiUrlRegionData)
-  .then(function(response) {
-    if (response.ok) {
-      response.json().then(function(text) {
-        console.log(text);
 
-        for(var i = 360; i < text.data.length; i++) {
-          var date = text.data[i].date;
-          if ( date === todaysDate) {
-            console.log(text.data[i]);
+    var apiUrlRegions = "https://api.covid19tracker.ca/regions";
+    fetch(proxyUrl + apiUrlRegions)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(text) {
+                    console.log(text);
+                    for (var i = 0; i < text.data.length; i++) {
+                        if (text.data[i].hr_uid == locationId) {
+                            $(".region-title").append(`${text.data[i].engname}`)
+                        };
+                    }
+                });
+            };
+        })
 
-            var totalCases = parseInt(text.data[i].total_cases);
-            var totalRecoveries = parseInt(text.data[i].total_recoveries);
-            var activeCases = totalCases - totalRecoveries;
-            $(".total-cases").append(`${text.data[i].total_cases}`);
-            $(".active-cases").append(activeCases);
-            $(".total-regionRecoveries").append(`${text.data[i].total_recoveries}`);
-          };
-        };
-      })
-    };
-  })
+    var apiUrlRegionData = `https://api.covid19tracker.ca/reports/regions/${locationId}`;
+    fetch(proxyUrl + apiUrlRegionData)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(text) {
+                    console.log(text);
+
+                    for (var i = 360; i < text.data.length; i++) {
+                        var date = text.data[i].date;
+                        if (date === todaysDate) {
+                            console.log(text.data[i]);
+
+                            var totalCases = parseInt(text.data[i].total_cases);
+                            var totalRecoveries = parseInt(text.data[i].total_recoveries);
+                            var activeCases = totalCases - totalRecoveries;
+                            $(".total-cases").text(`${text.data[i].total_cases}`);
+                            $(".active-cases").text(activeCases);
+                            $(".total-regionRecoveries").text(`${text.data[i].total_recoveries}`);
+                        };
+                    };
+                })
+            };
+        })
 };
 
 provincialData();
