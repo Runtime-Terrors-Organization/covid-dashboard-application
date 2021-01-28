42c8929807aad85124b3769c6d7381e33a0667c5
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-01-27 19:03:59 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {
-      lat: -34.397,
-      lng: 150.644
+      lat: 43.6532,
+      lng: -79.3832
     },
   zoom: 8,
   });
